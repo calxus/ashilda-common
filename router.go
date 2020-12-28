40e2577db4cccb8e969067cf541476be10b47c8f
Gordon Adam-Santiago <gtadam@protonmail.ch>
2020-12-28 16:12:10 +0000

    Exporting variables

--- router.go
+++ router.go
@@ -4,16 +4,16 @@ import "github.com/gorilla/mux"
 
 // Router type for holding mux router and database connection used in handlers
 type Router struct {
-	basePath string
-	database Database
-	mux      *mux.Router
+	BasePath string
+	Database Database
+	Mux      *mux.Router
 }
 
 // NewRouter method to construct Router type
 func NewRouter(bp string) *Router {
 	return &Router{
-		basePath: bp,
-		database: *NewDatabase(),
-		mux:      mux.NewRouter().StrictSlash(true),
+		BasePath: bp,
+		Database: *NewDatabase(),
+		Mux:      mux.NewRouter().StrictSlash(true),
 	}
 }
